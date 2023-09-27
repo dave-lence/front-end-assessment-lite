28c8f2273b66ef0bdd7597dcc7d2fb682c14bdfb
dave-lence <davidfnwudo@gmail.com>
2023-09-27 12:53:04 +0100

    edited demo data

--- my-product-assessment/DemoData/appData.js
+++ my-product-assessment/DemoData/appData.js
@@ -3,73 +3,73 @@ export const productData = [
         id: "1",
         price: 30.00,
         productName: "Vaserce Bag",
-        img: require("./assets/cart1.png")
+        img: require("../assets/cart1.png")
      },
     {
         id: "2",
         price: 50.00,
         productName: "Vaserce Bag",
-        img: require("./assets/cart2.png")
+        img: require("../assets/cart2.png")
      },
     {
         id: "3",
         price: 40.00,
         productName: "Vaserce Bag",
-        img: require("./assets/product2.png")
+        img: require("../assets/product2.png")
      },
     {
         id: "4",
         price: 100.00,
         productName: "Vaserce Bag",
-        img: require("./assets/product3.png")
+        img: require("../assets/product3.png")
      },
     {
         id: "5",
         price: 170.00,
         productName: "Vaserce Bag",
-        img: require("./assets/product3.png")
+        img: require("../assets/product3.png")
      },
     {
         id: "6",
         price: 10.00,
         productName: "Vaserce Bag",
-        img: require("./assets/product3.png")
+        img: require("../assets/product3.png")
      },
      {
         id: '7',
         price: 80.00,
         productName: "Vaserce Bag",
-        img: require("./assets/beans7.png")
+        img: require("../assets/beans7.png")
      },
     {
         id: '8',
         price: 200.00,
         productName: "Vaserce Bag",
-        img: require("./assets/beans3.png")
+        img: require("../assets/product3.png")
      },
     {
         id: '9',
         price: 120.00,
         productName: "Vaserce Bag",
-        img: require("./assets/burgitos.png")
+        img: require("../assets/burgitos.png")
      },
     {
         id: '10',
         price: 115.00,
         productName: "Vaserce Bag",
-        img: require("./assets/beans6.png")
+        img: require("../assets/beans6.png")
      },
       {
         id: '11',
         price: 135.00,
         productName: "Vaserce Bag",
-        img: require("./assets/cart2.png")
+        img: require("../assets/cart2.png")
      },
     {
         id: '12',
         price: 45.00,
         productName: "Vaserce Bag",
-        img: require("./assets/product6.png")
+        img: require("../assets/product6.png")
      },
     
 ]
